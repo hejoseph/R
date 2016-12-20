c6b055f8c514588e164eb8fc2c36a6ed1f0aaa48
StefanAngoso <stefan.angoso@gmail.com>
2016-12-20 11:57:59 +0100

    Code fonctionnel pour LASSO et Ridge

--- code_stefan/4.2lasso.R
+++ code_stefan/4.2lasso.R
@@ -1,19 +1,19 @@
 library("glmnet")
 library("ROCR")
 
-# Régression pénalisée
+# R?gression p?nalis?e
 
 x <- model.matrix(~.,train[1:24])[,-1]
 y <- train[,25]
 
-# Entrainement du modèle
-model2 <- cv.glmnet(x,y,family="binomial")
+# Entrainement du mod?le
+model2 <- cv.glmnet(x,y,family="binomial",alpha=1)
 
 coef(model2)
 
 xtest <- model.matrix(~.,test[1:24])[,-1]
 res2 <- predict(model2, xtest, type="class")
 
-mean(res2 == test[,25]) # précision
+mean(res2 == test[,25]) # pr?cision
 
 contrasts(test[,25])
---
+++ code_stefan/4.4ridge.R
@@ -0,0 +1,19 @@
+library("glmnet")
+library("ROCR")
+
+# R?gression p?nalis?e
+
+x <- model.matrix(~.,train[1:24])[,-1]
+y <- train[,25]
+
+# Entrainement du mod?le
+model2 <- cv.glmnet(x,y,family="binomial",alpha=0)
+
+coef(model2)
+
+xtest <- model.matrix(~.,test[1:24])[,-1]
+res2 <- predict(model2, xtest, type="class")
+
+mean(res2 == test[,25]) # pr?cision
+
+contrasts(test[,25])
