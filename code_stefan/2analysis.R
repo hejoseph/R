9fc92071da80203606d8a063d6805fae240dd454
StefanAngoso <stefan.angoso@gmail.com>
2016-12-20 17:54:59 +0100

    Changement de référence : nockd = 0 et ckd = 1

--- code_stefan/2analysis.R
+++ code_stefan/2analysis.R
@@ -6,3 +6,7 @@ pairs ( df[,nums])
 
 # Une autre façon d'afficher le test de corrélation entre les Variables quanti
 symnum(cor(df[,nums],use="complete"))
+
+# On donne pour référence notckd = 0, ckd = 1
+df$class<-relevel(df$class,ref="notckd")
+contrasts(df[,25])
